b8f731d924d5810bf1b432ddfec6f42c44f9fbb2
grovatti <guillermo.rovatti@gmail.com>
2022-01-12 20:25:40 -0300

    feat: Cambio en mi-app1.js

--- mi-app1.js
+++ mi-app1.js
@@ -2,3 +2,6 @@ var nombre = 'Juan';
 var nombre = 'Juan Jose';
 var nombre = 'Juan Jose Rodriguez'
 //console.log(nombre);  //daría como resultado Juan Jose Rodriguez
+
+let nombre1 = 'Juan';
+console.log(nombre1);
